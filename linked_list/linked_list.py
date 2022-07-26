125cd56f3e2ece4106ee115b2d077d6f7aba17dd
Cabebe <aricabebe@gmail.com>
2022-07-25 21:01:05 -0700

    finishes delete and reverse. All tests passing

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -27,10 +27,10 @@ class LinkedList:
     # Space Complexity: O(1)
     def add_first(self, value):
         if self.head is None:
-            self.head = Node(value, self.head)
-
-        new_node = Node(value, next_node = self.head)
-        self.head = new_node
+            self.head = Node(value)
+        else:
+            new_node = Node(value, next_node = self.head)
+            self.head = new_node
         
         
 
@@ -56,7 +56,7 @@ class LinkedList:
         if not self.head:
             return 0
         current = self.head
-        count = 0
+        count = 1
         while current: 
             if not current.next: 
                 return count
@@ -83,7 +83,7 @@ class LinkedList:
     # method that returns the value of the last node in the linked list
     # returns None if the linked list is empty
     # Time Complexity: O(n)
-    # Space Complexity: O(n)
+    # Space Complexity: O(1)
     def get_last(self):
         if not self.head: 
             return None
@@ -96,10 +96,11 @@ class LinkedList:
 
     # method that inserts a given value as a new last node in the linked list
     # Time Complexity: O(n)
-    # Space Complexity: 
+    # Space Complexity: O(1)
     def add_last(self, value):
         if not self.head: 
             self.head = Node(value, next_node = None)
+            return
         current = self.head
         while current: 
             if not current.next:
@@ -121,8 +122,8 @@ class LinkedList:
         return max_value
 
     # method to delete the first node found with specified value
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
     def delete(self, value):
         if not self.head:
             return
@@ -132,27 +133,20 @@ class LinkedList:
         
         current = self.head
         previous = None
-
         while current:
-            if current.next is None and current.value == value:
+            if not current.next and current.value == value:
                 previous.next = None
                 return
             if current.value == value:
-                if current.next is None:
-                    previous.next == None
                 previous.next = current.next
                 return 
             previous = current
             current = current.next
-            print(previous.value, current.value)
-            
-        
-        # if value isn't found, don't do anything
         
 
     # method to print all the values in the linked list
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
     def visit(self):
         helper_list = []
         current = self.head
@@ -168,7 +162,19 @@ class LinkedList:
     # Time Complexity: ?
     # Space Complexity: ?
     def reverse(self):
-        pass
+        if not self.head:
+            return 
+
+        previous = None
+        current = self.head
+        next = self.head.next
+        while next:
+            current.next = previous
+            previous = current
+            current = next
+            next = next.next
+        current.next = previous
+        self.head = current
   
     ## Advanced/ Exercises
     # returns the value at the middle element in the singly linked list
